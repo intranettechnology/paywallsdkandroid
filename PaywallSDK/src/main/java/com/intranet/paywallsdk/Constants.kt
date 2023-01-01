639973ba84d2ebb63ff2cb5e1b0e39329b4cdd9c
serkanintranet <serkan@intranettechnology.com>
2023-01-01 16:37:37 +0300

    01.01.2023

--- PaywallSDK/src/main/java/com/intranet/paywallsdk/Constants.kt
+++ PaywallSDK/src/main/java/com/intranet/paywallsdk/Constants.kt
@@ -1,7 +1,7 @@
 package com.intranet.paywallsdk
 
 object Constants {
-    const val BASE_URL = "https://devpayment.itspaywall.com/api/paywall/"
+    const val BASE_URL = "https://payment-api.itspaywall.com/api/paywall/"
     const val START3D = "payment/start3d"
     const val END3D = "payment/end3d"
     const val VERSION = "payment/version"
